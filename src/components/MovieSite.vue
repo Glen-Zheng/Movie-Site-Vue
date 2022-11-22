7d729dfc00aa765fe1a063b036e3680e730b905c
Glen Zheng <zhengglen200@gmail.com>
2022-11-21 20:33:11 -0500

    Made the Vue JS site basically the same as the Vanilla one. Small things to fix now.

--- src/components/MovieSite.vue
+++ src/components/MovieSite.vue
@@ -2,53 +2,27 @@
 import { ref } from "vue";
 import axios from "axios";
 
-// let movies;
-// let div;
-// let video ;
-// let removeFind;
-// let trailers ;
-// let title  ;
-// let poster ;
-// let overview ;
-// let releaseDate ;
-// let genre ;
-// let revenue ;
-// let budget;
-// let runtime ;
-// let tagline ;
-// let homepage ;
-// let ranking ;
-// let collection ;
-// let collectionPoster ;
-// let trailer ;
-
 let movies;
-// let div = ref(null);
-let video = ref(null);
-// let removeFind = ref(null);
+let video = ref();
 let removeIf = ref(false);
-let trailers = ref(null);
-let title = ref(null);
-let poster = ref(null);
-let overview = ref(null);
-let releaseDate = ref(null);
-let genre = ref(null);
-let revenue = ref(null);
-let budget = ref(null);
-let runtime = ref(null);
-let tagline = ref(null);
-let homepage = ref(null);
-let ranking = ref(null);
-let collection = ref(null);
-let collectionPoster = ref(null);
-let trailer = ref(null);
+let trailers = ref();
+let title = ref();
+let poster = ref();
+let overview = ref();
+let releaseDate = ref();
+let genre = ref();
+let revenue = ref();
+let budget = ref();
+let runtime = ref();
+let tagline = ref();
+let homepage = ref();
+let ranking = ref();
+let collection = ref();
+let collectionPoster = ref();
+let trailer = ref();
 let collectionTF = ref(false);
 
 async function movieOutput() {
-
-  // div = document.createElement("div");
-  // div.setAttribute("id", "remover");
-  // document.body.append(div);
   removeIf.value = true;
 
   video = await axios.get(`https://api.themoviedb.org/3/movie/${movies}`, {
@@ -61,32 +35,10 @@ async function movieOutput() {
   trailers = video.data.videos.results.filter(
     (trailer) => trailer.type === "Trailer"
   );
+  if (video.data.belongs_to_collection) {
+     collectionTF.value = true;
+    }
 
-  // title = document.createElement("h1");
-  // title.setAttribute("id", "movie-title");
-  // poster = document.createElement("img");
-  // poster.setAttribute("id", "movie-poster");
-  // overview = document.createElement("p");
-  // overview.setAttribute("id", "movie-overview");
-  // releaseDate = document.createElement("p");
-  // releaseDate.setAttribute("id", "movie-release-date");
-  // genre = document.createElement("p");
-  // genre.setAttribute("id", "movie-genre");
-  // revenue = document.createElement("p");
-  // revenue.setAttribute("id", "movie-revenue");
-  // budget = document.createElement("p");
-  // budget.setAttribute("id", "movie-budget");
-  // runtime = document.createElement("p");
-  // runtime.setAttribute("id", "movie-runtime");
-  // tagline = document.createElement("h2");
-  // tagline.setAttribute("id", "movie-tagline");
-  // homepage = document.createElement("a");
-  // homepage.setAttribute("id", "movie-homepage");
-  // homepage.setAttribute("target", "_blank");
-  // ranking = document.createElement("p");
-  // ranking.setAttribute("id", "movie-ranking");
-  // trailer = document.createElement("iframe");
-  // trailer.setAttribute("id", "movie-trailer");
 
   title.value.innerHTML = `${video.data.title} `;
   poster.value.src = `https://image.tmdb.org/t/p/w500${video.data.poster_path}`;
@@ -102,119 +54,57 @@ async function movieOutput() {
   ranking.value.innerHTML = `Ranking: ${video.data.vote_average}/10`;
   trailer.value.src = `https://www.youtube.com/embed/${trailers[0].key}`;
 
-  // div.append(title);
-  // div.append(poster);
-  // div.append(overview);
-  // div.append(releaseDate);
-  // div.append(genre);
-  // div.append(revenue);
-  // div.append(budget);
-  // div.append(runtime);
-  // div.append(tagline);
-  // div.append(homepage);
-  // div.append(ranking);
-  if (video.data.belongs_to_collection) {
 
-    collectionTF.value = true;
-
-    // collection = document.createElement("p");
-    // collection.setAttribute("id", "movie-collection");
-    // collectionPoster = document.createElement("img");
-    // collectionPoster.setAttribute("id", "movie-collection-poster");
-    collection.value.innerHTML = `${video.data.belongs_to_collection.name}`;
-    collectionPoster.value.src = `https://image.tmdb.org/t/p/w500${video.data.belongs_to_collection.poster_path}`;
-    // div.append(collection);
-    // div.append(collectionPoster);
-  }
-  // div.append(trailer);
-
-  // removeFind = document.getElementById("remover");
-
-
+  collection.value.innerHTML = `${video.data.belongs_to_collection.name}`;
+  collectionPoster.value.src = `https://image.tmdb.org/t/p/w500${video.data.belongs_to_collection.poster_path}`;
 }
 </script>
 
 <template>
+  <div id="main-div">
+    <h1 id="pagetitle">Movie Finder</h1>
+    <p id="intro">See the drop down below to select a movie</p>
+    <form>
+      <label id="moviesdropdownlabel" for="moviesdropdown"
+        >Choose a movie:
+      </label>
+      <select id="moviesdropdown" v-model="movies">
+        <option value="424139">Halloween</option>
+        <option value="354912">Coco</option>
+        <option value="458156">John Wick: Chapter 3 - Parabellum</option>
+        <option value="146233">Prisoners</option>
+        <option value="399566">Godzilla vs. Kong</option>
+        <option value="72190">World War Z</option>
+        <option value="1422">The Departed</option>
+        <option value="12405">Slumdog Millionaire</option>
+        <option value="27205">Inception</option>
+        <option value="396371">Molly's Game</option>
+      </select>
+    </form>
+    <div class="alignbutton">
+      <button id="get" @click="movieOutput()">Get Movie!</button>
+    </div>
 
-  <!-- use something like this to style the text, to have it out 
-  in the template so that it can be styled in the <style> below. Having it will also mean
-  the code is cleaned up to be vue. and that the refs have everything. 
-  UNDerstand the goal -->
-  <!--is the solution to use refs ( call it in the attributes) and then it'll update? only when get is pressed the info is updated-->
-
-  <!-- <div id="movieProfile">
-        <img id="poster" src="" ref="poster">
-        <h1 id="title" ref="title"></h1>
-        <h3 id="tagline" ref="tagline"></h3>
-        <h3 id="status" ref="status"></h3>
-        <p id="popularity" ref="popularity"></p>
-        <p id="voteAverage" ref="voteAverage"></p>
-        <p id="voteCount" ref="voteCount"></p>
-        <p id="budget" ref="budget"></p>
-        <p id="overview" ref="overview"></p>
-
-    </div> -->
-
-<div id="main-div">
-
-
-  <h1 id="pagetitle">Movie Finder</h1>
-  <p id="intro">See the drop down below to select a movie</p>
-  <form>
-    <label id="moviesdropdownlabel" for="moviesdropdown"
-      >Choose a movie:
-    </label>
-    <select id="moviesdropdown" v-model="movies">
-      <option value="424139">Halloween</option>
-      <option value="354912">Coco</option>
-      <option value="458156">John Wick: Chapter 3 - Parabellum</option>
-      <option value="146233">Prisoners</option>
-      <option value="399566">Godzilla vs. Kong</option>
-      <option value="72190">World War Z</option>
-      <option value="1422">The Departed</option>
-      <option value="12405">Slumdog Millionaire</option>
-      <option value="27205">Inception</option>
-      <option value="396371">Molly's Game</option>
-    </select>
-  </form>
-  <div class="alignbutton">
-    <button id="get" @click="movieOutput()" >Get Movie!</button>
-  </div>
-
-
-  <div id="remover" v-if="removeIf">
-    <img id = "movie-poster" src =" " ref= "poster"/>
-    <p id="movie-title" ref= "title"></p>
-    <p id="movie-overview" ref= "overview"></p>
-    <p id="movie-release-date" ref= "releaseDate"></p>
-    <p id="movie-genre" ref= "genre"></p>
-    <p id="movie-revenue" ref= "revenue"></p>
-    <p id="movie-budget" ref= "budget"></p>
-    <p id="movie-runtime" ref= "runtime" ></p>
-    <p id="movie-tagline" ref= "tagline"></p>
-    <a id="movie-homepage" target="_blank" href= " " ref= "homepage"></a>
-    <iframe id ="movie-trailer" src=" " ref= "trailer"></iframe>
-    <p id ="movie-ranking" ref= "ranking"></p>
-
-    <div v-if="collectionTF">
-    <p id ="movie-collection" ref="collection"></p>
-    <img id="movie-collection-poster" src = " " ref="collectionPoster" />
+    <div id="remover" v-if="removeIf">
+      <img id="movie-poster" src=" " ref="poster" />
+      <p id="movie-title" ref="title"></p>
+      <p id="movie-overview" ref="overview"></p>
+      <p id="movie-release-date" ref="releaseDate"></p>
+      <p id="movie-genre" ref="genre"></p>
+      <p id="movie-revenue" ref="revenue"></p>
+      <p id="movie-budget" ref="budget"></p>
+      <p id="movie-runtime" ref="runtime"></p>
+      <p id="movie-tagline" ref="tagline"></p>
+      <a id="movie-homepage" target="_blank" href=" " ref="homepage"></a>
+      <iframe id="movie-trailer" src=" " ref="trailer"></iframe>
+      <p id="movie-ranking" ref="ranking"></p>
+      <p id="movie-collection" ref="collection" v-if="collectionTF"></p>
+      <img id="movie-collection-poster" src=" " ref="collectionPoster" v-if="collectionTF" />
     </div>
   </div>
-
-</div>
-
 </template>
 
 <style scoped>
-
-
-/* #topdiv {
-  background-color: #C9A9A6;
-  width:100%;
-  aspect-ratio: 10/1;
-} */
-
 #pagetitle {
   font-family: "Rye", cursive;
   font-size: 80px;
@@ -261,6 +151,8 @@ async function movieOutput() {
   font-size: 70px;
   color: #29e7d7;
   background-color: #9429f2;
+  margin-top: 2rem;
+  margin-bottom: 2.1rem;
 }
 
 #get:hover {
@@ -268,6 +160,10 @@ async function movieOutput() {
   color: rgb(22, 22, 144);
   transform: scale(1.5);
   transition-duration: 2s;
+}
+
+#get:focus {
+  border: 2px solid white;
 }
 
 #remover {
@@ -311,6 +207,8 @@ async function movieOutput() {
 }
 
 #movie-tagline {
+  font-weight: bold;
+  font-size: 1.5rem;
   grid-row: 2/3;
   grid-column: 2/5;
   align-self: center;
@@ -404,20 +302,15 @@ async function movieOutput() {
   15% {
     color: rgb(222, 128, 22);
   }
-
-  /* 30%  {color:blue; } */
   45% {
     color: green;
   }
   50% {
     color: black;
   }
-  /* 60%{color:red; } */
-  /* 75%  {color:violet; } */
   80% {
     color: aqua;
   }
-  /* 100% {color:bisque;}  */
 }
 .animate {
   animation-name: ani;
